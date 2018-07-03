ef934666d28cbf0486ce05ac37c216f2996bea2b
Cristian Gartner <gartner90@gmail.com>
2018-07-02 19:15:53 -0500

    Get request reddit api, test data return

--- reddit/src/App.js
+++ reddit/src/App.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import { Row, Col } from 'react-bootstrap';
 import List from './components/List';
 import Detail from './components/Detail';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    
+    this.getData();
+  }
+
+  getData() {
+    axios.get('https://www.reddit.com/r/funny/top.json?limit=50')
+      .then(response => {
+        const data = response.data.data.children;
+        console.log('data', data);
+      })
+  }
+
   render() {
     return (
       <div className="container-fluid">
